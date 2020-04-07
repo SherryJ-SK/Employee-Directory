d375bf50fe4dff74d6ff04b09bd9ec1f4a515d9e
Sherry <a-sherry@live.com>
2020-04-07 20:02:24 +1000

    Updates

---
+++ precache-manifest.d24f1e8c9ea7d49a800b591c4044fbed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f3b95616ff731846973be4c6f9e8ecc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "087fbc87f8978937e53d",
+    "url": "/static/css/main.229b86d3.chunk.css"
+  },
+  {
+    "revision": "8f0b61117843637433f3",
+    "url": "/static/js/2.91f65a50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.91f65a50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "087fbc87f8978937e53d",
+    "url": "/static/js/main.c01634bc.chunk.js"
+  },
+  {
+    "revision": "afbff23e4e4900217f09",
+    "url": "/static/js/runtime-main.2aeab658.js"
+  }
+]);
